a4cfa6208b1a10e9d770cd94794ed32bb61f44d0
chakru-hero <chakradhar.dwivedi2000@gmail.com>
2023-11-26 00:38:29 +0530

    Removed comments

--- src/main/java/com/beta/replyservice/RestServiceApplication.java
+++ src/main/java/com/beta/replyservice/RestServiceApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestServiceApplication {
-//	@Autowired
-//	private static ApplicationContext applicationContext;
 	public static void main(String[] args) {
-//		Map<String,HashAlgoService> map = applicationContext.getBeansOfType(HashAlgoService.class);
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
 
